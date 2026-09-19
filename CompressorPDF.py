071e0e15aacbf1b028e4a89e435ad91b054ee411
AndresEsp-Dev <annddy0365@gmail.com>
2026-09-18 20:02:44 -0500

    Fix column layout for download results section

--- CompressorPDF.py
+++ CompressorPDF.py
@@ -192,7 +192,7 @@ with st.container():
         if st.session_state.processed_files:
             st.markdown("---")
             # CORREGIDO AQUÍ: se pasan proporciones
-            col_title, col_action = st.columns()
+            col_title, col_action = st.columns(2) 
             with col_title:
                 st.subheader("📦 Resultados Listos para Descargar")
                 
